ec8f013b04851dfbc1075ced8816d467383f589d
Bionic_Leha <bionic.mods@gmail.com>
2015-03-12 23:00:56 +0200

    Initital

---
+++ app/src/main/java/com/bionic/icons/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bionic.icons;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
